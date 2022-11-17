9ef469f1d073a4b6c751f3f0cab23ca3a285ebae
Joseph Zins <jzins1@css.edu>
2022-11-17 10:22:56 -0600

    Learning Activity 11 Part 1

---
+++ AbstractTrader/ITradeProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractTrader
+{
+    public interface ITradeProcessor
+    {
+
+        void ProcessTrades(Stream stream);
+
+
+    }
+}
